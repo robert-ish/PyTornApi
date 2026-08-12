c19ba33680914950143fc059fd0857b2fd769358
robert-ish <ishbuldin.robert.3000@gmail.com>
2026-08-12 14:14:44 +0300

    updating...

--- tornapi/exceptions.py
+++
@@ -1,8 +0,0 @@
-class TornAPIError(Exception):
-    pass
-class RateLimitError(TornAPIError):
-    pass
-class InvalidKeyError(TornAPIError):
-    pass
-class TornAPIResponseError(TornAPIError):
-    pass
