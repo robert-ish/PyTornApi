269e510fcf90a498ec659a3e22aff772c71cb514
robert-ish <ishbuldin.robert.3000@gmail.com>
2026-08-12 20:20:16 +0300

    Update ratelimithandling.py

--- examples/ratelimithandling.py
+++ examples/ratelimithandling.py
@@ -1,9 +1,9 @@
 # handles rate limits but might be a bit inconvinient since you have to wait.
-import PyTornApi
+import PyTornApi as pt
 
 def try_call(func, *args):
   try:
     func(*args)
-  except RateLimitError as e:
+  except pt.RateLimitError as e:
     time.sleep(e.retry_after) # yes, you can do this
     func(*args)
